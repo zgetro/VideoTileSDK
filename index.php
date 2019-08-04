508867dfb72499dc52243434e73c43df36a3cd23
Craig Sledmore <sledmore2012@gmail.com>
2019-08-04 20:17:35 +0100

    Code refactors

--- index.php
+++ index.php
@@ -1,21 +1,28 @@
 <?php
 
-use \GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\GuzzleException;
+use Psr\Http\Message\StreamInterface;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7;
 
 class VideoTile
 {
-    private $client = null;
-    private $endpoint = null;
+    private $_client = null;
+    private $_endpoint = null;
+    private $_adminToken = '';
 
     /**
      * VideoTileAPI constructor.
-     * @param $endpoint
+     * @param string $endpoint The full URL to the API.
+     * @param string $adminToken Private admin token used for authorization on the API endpoint.
      */
-    public function __construct($endpoint)
+    public function __construct($endpoint, $adminToken)
     {
-        $this->endpoint = $endpoint;
+        $this->_endpoint = $endpoint;
+        $this->_adminToken = $adminToken;
 
-        $this->client = new GuzzleClient([
+        $this->_client = new GuzzleClient([
             'http_errors' => false
         ]);
     }
@@ -23,32 +30,32 @@ class VideoTile
     /**
      * Returns a JSON array of all the courses stored with VideoTile.
      *
-     * @return \Psr\Http\Message\StreamInterface
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @return StreamInterface
+     * @throws GuzzleException
      */
     public function getCourses()
     {
-        return $this->request('POST', '/courses');
+        return $this->request('POST', 'courses');
     }
 
     /**
      * Returns a JSON array of a specific course stored with VideoTile.
      *
      * @param integer $courseId
-     * @return \Psr\Http\Message\StreamInterface
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @return StreamInterface
+     * @throws GuzzleException
      */
     public function getCourseById($courseId)
     {
-        return $this->request('POST', '/courses/' . $courseId);
+        return $this->request('POST', 'courses/' . $courseId);
     }
 
     /**
      * Returns a JSON array of a all courses associated with a user.
      *
      * @param string $token User associated API token, required for authorization
-     * @return \Psr\Http\Message\StreamInterface
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @return StreamInterface
+     * @throws GuzzleException
      */
     public function getMyCourses($token)
     {
@@ -62,15 +69,14 @@ class VideoTile
     /**
      * Returns a JSON array of all users associated with VideoTile.
      *
-     * @param string $token User associated API token, required for authorization
-     * @return false|\Psr\Http\Message\StreamInterface|string
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @return false|StreamInterface|string
+     * @throws GuzzleException
      */
-    public function getUserList($token)
+    public function getUserList()
     {
         return $this->request('POST', 'admin/users/list', [
             'form_params' => [
-                'admin_token' => $token
+                'admin_token' => $this->_adminToken
             ]
         ]);
     }
@@ -78,16 +84,15 @@ class VideoTile
     /**
      * Returns a JSON object of a user.
      *
-     * @param string $token User associated API token, required for authorization
      * @param $userId
-     * @return false|\Psr\Http\Message\StreamInterface|string
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @return false|StreamInterface|string
+     * @throws GuzzleException
      */
-    public function getUserById($token, $userId)
+    public function getUserById($userId)
     {
         return $this->request('POST', 'admin/users/user/' . $userId, [
             'form_params' => [
-                'admin_token' => $token
+                'admin_token' => $this->_adminToken
             ]
         ]);
     }
@@ -95,16 +100,15 @@ class VideoTile
     /**
      * Returns an array of all courses associated to a user.
      *
-     * @param string $token User associated API token, required for authorization
      * @param integer $userId
-     * @return false|\Psr\Http\Message\StreamInterface|string
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @return false|StreamInterface|string
+     * @throws GuzzleException
      */
-    public function getUserCoursesById($token, $userId)
+    public function getUserCoursesById($userId)
     {
         return $this->request('POST', 'admin/users/user/' . $userId . '/courses', [
             'form_params' => [
-                'admin_token' => $token
+                'admin_token' =>  $this->_adminToken
             ]
         ]);
     }
@@ -112,17 +116,16 @@ class VideoTile
     /**
      * Returns a user object, course object and user course object.
      *
-     * @param string $token Admin API token, required for authorization
-     * @param integer $userId
-     * @param integer $courseId
-     * @return \Psr\Http\Message\StreamInterface
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @param integer $userId Id of the user to preform the action on.
+     * @param integer $courseId Id of the course.
+     * @return StreamInterface
+     * @throws GuzzleException
      */
-    public function getUserCourseByCourseId($token, $userId, $courseId)
+    public function getUserCourseByCourseId($userId, $courseId)
     {
         return $this->request('POST', 'admin/users/user/' . $userId . '/courses/' . $courseId, [
             'form_params' => [
-                'admin_token' => $token
+                'admin_token' => $this->_adminToken
             ]
         ]);
     }
@@ -130,16 +133,15 @@ class VideoTile
     /**
      * Returns a status code.
      *
-     * @param string $token
-     * @param integer $userId
-     * @return \Psr\Http\Message\StreamInterface
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @param integer $userId Id of the user to preform the action on.
+     * @return StreamInterface
+     * @throws GuzzleException
      */
-    public function disableUserAccountById($token, $userId)
+    public function disableUserAccountById($userId)
     {
         return $this->request('POST', 'admin/users/user/' . $userId . '/disable', [
             'form_params' => [
-                'admin_token' => $token
+                'admin_token' => $this->_adminToken
             ]
         ]);
     }
@@ -147,13 +149,13 @@ class VideoTile
     /**
      * Returns a status code.
      *
-     * @param string $token
-     * @return \Psr\Http\Message\StreamInterface
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @param string $token A users API token
+     * @return StreamInterface
+     * @throws GuzzleException
      */
     public function generateAuthToken($token)
     {
-        return $this->request('POST', '/my/lms-login', [
+        return $this->request('POST', 'my/lms-login', [
             'form_params' => [
                 'api_token' => $token
             ]
@@ -161,26 +163,26 @@ class VideoTile
     }
 
     /**
-     * @param integer $status
-     * @param string $endPoint
-     * @param array $parameters
-     * @return false|\Psr\Http\Message\StreamInterface|string
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @param string $verb HTTP Verb (POST, GET, PUT, PATCH, DELETE)
+     * @param string $resource The API resource we're sending a request to.
+     * @param array $parameters An array of parameters for the request.
+     * @return false|StreamInterface|string
+     * @throws GuzzleException
      */
-    private function request($status, $endPoint, $parameters = [])
+    private function request($verb, $resource, $parameters = [])
     {
         header('Content-Type: application/json');
 
-        $response = $this->client->request($status, $this->endpoint . '/' . $endPoint, $parameters);
+        try {
+            $response = $this->_client->request($verb, ($this->_endpoint . '/' . $resource), $parameters);
 
-        $status = $response->getStatusCode();
-
-        if ($status == 200) {
             return $response->getBody();
-        }
+        } catch (RequestException $e) {
+            echo Psr7\str($e->getRequest());
 
-        return json_encode([
-            'error' => 'soething'
-        ]);
+            if ($e->hasResponse()) {
+                echo Psr7\str($e->getResponse());
+            }
+        }
     }
 }
--- usage.php
+++ usage.php
@@ -1,8 +1,12 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require __DIR__ . '/index.php';
 require __DIR__ . '/vendor/autoload.php';
 
-$api = new VideoTile('http://vt-api.local/');
+$api = new VideoTile('http://vt-api.local', 'WISHADMIN');
 
-echo $api->getUserById('WISHADMIN', 394);
+echo $api->getUserById(394);
