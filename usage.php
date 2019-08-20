73f801bcd688f4705b5a1e6566a0390c14d06ee7
Craig Sledmore <Sledmore@users.noreply.github.com>
2019-08-20 15:33:50 +0100

    Delete usage.php

--- usage.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require __DIR__ . '/index.php';
-require __DIR__ . '/vendor/autoload.php';
-
-$api = new VideoTile('http://vt-api.local', 'admin_token', 'vendor_lms_name');
-
-echo $api->generateLoginUrl('user_token');
