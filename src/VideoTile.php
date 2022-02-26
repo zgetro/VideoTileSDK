54016c3d2edf8b5627cae69860fb035f42a36dc5
panchania83 <95633879+panchania83@users.noreply.github.com>
2022-02-26 22:41:05 +0530

    as per document assign user

--- src/VideoTile.php
+++ src/VideoTile.php
@@ -365,7 +365,10 @@ class VideoTile
             'admin/course/assign',
             [
                 'form_params' => [
+                    'user_id' => $userId,
+                    'course_id' => $courseId,
                     'admin_token' => $this->_adminToken,
+                    'vendor_id' => $this->_vendor,
                 ],
             ]
         );
